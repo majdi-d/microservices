9e52a29aa9f99ce83a5787b993285f4a90ed4a45
Majdi Dhissi <majdi.dhissi@gmail.com>
2022-12-05 02:30:01 +0400

    Added Redis cache

---
+++ microservices.customersvc/Controllers/CacheController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using StackExchange.Redis;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace microservices.customersvc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        //private readonly IConnectionMultiplexer _redisCache;
+        private readonly IDistributedCache _distributedCache;
+        public CacheController(IDistributedCache distributedCache)
+        {
+            _distributedCache  = distributedCache;
+        }
+        // GET api/<CacheController>/5
+        [HttpGet]
+        public string? Get([FromQuery] string key)
+        {
+            return _distributedCache.GetString(key);
+            //return "value";
+        }
+
+        // POST api/<CacheController>
+        [HttpPost]
+        public void Post([FromBody] KeyValuePair<string,string> item)
+        {
+            _distributedCache.SetString(item.Key,item.Value);
+        }
+
+        
+    }
+}
--- microservices.customersvc/Program.cs
+++ microservices.customersvc/Program.cs
@@ -1,6 +1,13 @@
 using microservices.customersvc;
+using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.DependencyInjection;
 using Prometheus;
+using StackExchange.Redis;
+using System.Configuration;
+using System.Runtime.Intrinsics.X86;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,9 +23,41 @@ var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
 var dbName = Environment.GetEnvironmentVariable("DB_NAME");
 var dbUserName = "sa";
 var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var redis_host = Environment.GetEnvironmentVariable("REDIS_HOST");
+var redis_port = Environment.GetEnvironmentVariable("REDIS_PORT");
+var redis_password = Environment.GetEnvironmentVariable("REDIS_PASSWORD");
 var connectionString = $"Server={dbHost},1433;Database={dbName};User Id={dbUserName};Password={dbPassword};TrustServerCertificate=true;";
-builder.Services.AddDbContext<CustomerDbContext>(opt => opt.UseSqlServer(connectionString));
+
 // end dependency injection
+//var redisConfiguration = new ConfigurationOptions
+//{
+//    EndPoints = { "redis:6379" },
+//    Password = "eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81",
+//    AbortOnConnectFail = false,
+//    Ssl = false
+//};
+//var redis = ConnectionMultiplexer.Connect(redisConfiguration);
+//builder.Services.BuildServiceProvider();
+builder.Services.AddDbContext<CustomerDbContext>(opt => opt.UseSqlServer(connectionString))
+                .AddStackExchangeRedisCache(action =>
+                {
+                    action.InstanceName = "MyRedisCache";
+                    action.Configuration = $"{redis_host}:{redis_port}";
+                    action.ConfigurationOptions = new ConfigurationOptions
+                    {
+                        EndPoints = { $"{redis_host}:{redis_port}" },
+                        Password = redis_password,
+                        AbortOnConnectFail = false,
+                        Ssl = false
+                    };
+                });
+
+
+//var cache = serviceProvider.GetRequiredService<IDistributedCache>();
+// Dependency Injection
+
+
+// End Dependency injection
 
 var app = builder.Build();
 
